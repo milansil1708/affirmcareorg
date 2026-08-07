55e11d5666c6bd5f50de1860484c48acc71a424e
milansil1708 <milansil1708@gmail.com>
2026-08-07 19:30:27 -0400

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("providers/", views.ProviderResultsView.as_view(), name="provider_results"),
     path("about/", views.about_view, name="about"),
-    path("insurance/", views.insurance_view, name="insurance"),
+    path("news/", views.news_view, name="news"),
 ]
